44e57ee74ab7a81f123b5f49dcb7154797052e8e
Silverbeet <haphka1@live.no>
2020-09-20 17:16:48 +0200

    updated dependencies

--- server/Model/Person.py
+++ server/Model/Person.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisissecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///<PathToDb>'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TS-PY/Database/person.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
